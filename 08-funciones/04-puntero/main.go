12abb198657565eab526b178c510f8e2d707d35a
Tomas <viltetomas2003@gmail.com>
2023-03-02 17:17:07 -0300

    punteros

--- 08-funciones/04-puntero/main.go
+++ 08-funciones/04-puntero/main.go
@@ -1,1 +1,18 @@
-package _4_puntero
+package main
+
+import "fmt"
+
+func modicarValor(cadena *string) {
+	fmt.Printf("%p\n", cadena)
+	*cadena = "Hola desde la funcion"
+}
+
+func main() {
+	cadena := "Hola mundo de Go"
+	fmt.Printf("%p\n", &cadena)
+	fmt.Println("Antes de la funcion", cadena)
+
+	modicarValor(&cadena) // Referencia
+
+	fmt.Println("Despues de la funcion", cadena)
+}
