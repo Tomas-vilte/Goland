80bb595a86e105d265a75869c768ea0f78ecb635
Tomas <viltetomas2003@gmail.com>
2023-03-06 16:51:25 -0300

    defer panic and recover

--- 08-funciones/06-variable-global/main.go
+++ 08-funciones/06-variable-global/main.go
@@ -1,1 +1,24 @@
-package _6_variable_global
+package main
+
+import "fmt"
+
+// Variable global
+var mensaje string
+
+func funcion1() {
+	mensaje = "Hola desde la funcion uno!"
+	fmt.Println(mensaje)
+}
+
+func funcion2() {
+	mensaje = "Hola desde la funcion dos!"
+	fmt.Println(mensaje)
+}
+func main() {
+	mensaje = "Hola desde la funcion main!"
+	fmt.Println(mensaje)
+
+	defer funcion1()
+	funcion2()
+	fmt.Println(mensaje)
+}
--- 08-funciones/07-defer-panic-recover/main.go
+++ 08-funciones/07-defer-panic-recover/main.go
@@ -1,1 +1,29 @@
-package _7_defer_panic_recover
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Funcion
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Panic ocurred:", err)
+		}
+	}()
+
+	if file, err := os.Open("hola.txt"); err != nil {
+		panic("No fue posible obtener el archivo!!")
+	} else {
+		defer func() {
+			fmt.Println("Cerrando el archivo")
+			file.Close()
+		}()
+		contenido := make([]byte, 254)
+		long, _ := file.Read(contenido)
+		contenidoArchivo := string(contenido[:long])
+		fmt.Println(contenidoArchivo)
+	}
+
+}
