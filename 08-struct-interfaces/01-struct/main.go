14179636b9d8feb60a7e802368803a6321c42b06
Tomas <viltetomas2003@gmail.com>
2023-03-07 13:00:38 -0300

    Ejercicio tareas

--- 08-struct-interfaces/01-struct/main.go
+++ 08-struct-interfaces/01-struct/main.go
@@ -1,1 +1,48 @@
-package _1_struct
+package main
+
+import "fmt"
+
+// Struct persona
+
+type Persona struct {
+	nombre string
+	edad   int
+}
+
+// Metodo
+
+func (p *Persona) imprimir() {
+	fmt.Printf("Nombre: %s\nEdad:%d\n", p.nombre, p.edad)
+}
+
+func (p *Persona) editarNombre(nombre string) {
+	p.nombre = nombre
+}
+
+// Herencia
+type Empleado struct {
+	Persona
+	sueldo float64
+}
+
+func main() {
+	persona1 := Persona{"Thomas", 19}
+	persona1.imprimir()
+	//fmt.Println(persona1)
+	persona1.editarNombre("Alexis")
+	persona1.imprimir()
+	persona2 := Persona{
+		nombre: "Joan",
+		edad:   25,
+	}
+	//fmt.Println(persona2)
+	persona2.imprimir()
+	persona2.editarNombre("Emilse")
+	persona2.imprimir()
+
+	emp1 := Empleado{sueldo: 60.656}
+	emp1.nombre = "Lucas"
+	emp1.edad = 29
+	emp1.imprimir()
+	fmt.Println(emp1)
+}
--- 08-struct-interfaces/02-lista-tareas/tarea.go
+++ 08-struct-interfaces/02-lista-tareas/tarea.go
@@ -1,1 +1,61 @@
-package _2_lista_tareas
+package main
+
+import "fmt"
+
+// Lista de tareas
+type TaskList struct {
+	tasks []*Task
+}
+
+func (tl *TaskList) appendTask(t *Task) {
+	tl.tasks = append(tl.tasks, t)
+}
+
+func (tl *TaskList) deleteTask(index int) {
+	tl.tasks = append(tl.tasks[:index], tl.tasks[index+1:]...)
+}
+
+type Task struct {
+	name      string
+	desc      string
+	completed bool
+}
+
+func (t *Task) toPrint() {
+	fmt.Printf("Nombre: %s\nDescripcion: %s\nCompleted: %t\n", t.name, t.desc, t.completed)
+}
+
+func (t *Task) markCompleted() {
+	t.completed = true
+}
+
+func main() {
+	t1 := Task{
+		name:      "Curso de Go",
+		desc:      "Completar curso de Go este mes",
+		completed: false,
+	}
+	t2 := Task{
+		name:      "Curso de HTML",
+		desc:      "Completar curso de HTML esta semana",
+		completed: true,
+	}
+	lista := TaskList{}
+	lista.appendTask(&t1)
+	lista.appendTask(&t2)
+	t3 := Task{
+		name:      "Curso de CSS",
+		desc:      "Completar curso de CSS esta semana",
+		completed: false,
+	}
+	lista.appendTask(&t3)
+	fmt.Println(lista)
+
+	lista.deleteTask(1)
+
+	for i, task := range lista.tasks {
+		fmt.Println(i, task.name)
+	}
+	//t1.toPrint()
+	//t2.toPrint()
+}
